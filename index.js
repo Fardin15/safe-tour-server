f68e237c12465022a0de3efa05de591b5cf4a28f
Fardin15 <mohammedshowrav15@gmail.com>
2024-04-28 23:35:24 +0600

    data deleted successfully

--- index.js
+++ index.js
@@ -79,6 +79,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/addspot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
